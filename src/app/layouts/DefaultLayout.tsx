34b642d21bba7ac02fd96f70b6e557b5eeac0363
echu-mohan <156306645+echu-mohan@users.noreply.github.com>
2024-12-04 10:56:23 +0100

    Added Header.tsx, Task.tsx, DefaultLayout.tsx
    * Header.tsx is for setting up a common header
    * Task.tsx - is a component for adding the tasks
    * DefaultLayout.tsx - is for setting up a common layout for all pages.

---
+++ src/app/layouts/DefaultLayout.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+import Header from "../components/Header";
+
+interface Props {
+  children: ReactNode;
+}
+
+const DefaultLayout = ({ children }: Props) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <footer>
+        <p>&copy; 2022 My Application</p>
+      </footer>
+    </div>
+  );
+};
+
+export default DefaultLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Head from "next/head";
+import Task from "./components/Task";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 export default function Home() {
-  return <h1 className="flex justify-center mt-8 ">Welcome</h1>;
+  return (
+    <DefaultLayout>
+      <Head>
+        <title>My Application - Home</title>
+      </Head>
+
+      <h1 className="flex justify-center mt-8 ">Welcome</h1>
+
+      <Task />
+    </DefaultLayout>
+  );
 }
